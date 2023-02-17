5a3c88d7b7398d42702bf396501bf63bb282fb64
donskytech <donsky.iot.tech@gmail.com>
2023-02-17 13:59:27 +0800

    Initial Code

---
+++ index.js
@@ -0,0 +1,352 @@
+// Target specific HTML items
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+// Holds the background color of all chart
+var chartBGColor = getComputedStyle(document.body).getPropertyValue(
+  "--chart-background"
+);
+var chartFontColor = getComputedStyle(document.body).getPropertyValue(
+  "--chart-font-color"
+);
+var chartAxisColor = getComputedStyle(document.body).getPropertyValue(
+  "--chart-axis-color"
+);
+
+/*
+  Event listeners for any HTML click
+*/
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+
+  // Update Chart background
+  chartBGColor = getComputedStyle(document.body).getPropertyValue(
+    "--chart-background"
+  );
+  chartFontColor = getComputedStyle(document.body).getPropertyValue(
+    "--chart-font-color"
+  );
+  chartAxisColor = getComputedStyle(document.body).getPropertyValue(
+    "--chart-axis-color"
+  );
+  updateChartsBackground();
+});
+
+/*
+  Plotly.js graph and chart setup code
+*/
+var temperatureHistoryDiv = document.getElementById("temperature-history");
+var humidityHistoryDiv = document.getElementById("humidity-history");
+
+var temperatureGaugeDiv = document.getElementById("temperature-gauge");
+var humidityGaugeDiv = document.getElementById("humidity-gauge");
+
+const historyCharts = [temperatureHistoryDiv, humidityHistoryDiv];
+
+const gaugeCharts = [temperatureGaugeDiv, humidityGaugeDiv];
+
+// History Data
+var temperatureTrace = {
+  x: [],
+  y: [],
+  name: "Temperature",
+  mode: "lines+markers",
+  type: "line",
+};
+var humidityTrace = {
+  x: [],
+  y: [],
+  name: "Humidity",
+  mode: "lines+markers",
+  type: "line",
+};
+
+var temperatureLayout = {
+  autosize: true,
+  title: {
+    text: "Temperature",
+  },
+  font: {
+    size: 12,
+    color: chartFontColor,
+    family: "poppins, san-serif",
+  },
+  colorway: ["#05AD86"],
+  margin: { t: 40, b: 40, l: 60, r: 60, pad: 10 },
+  plot_bgcolor: chartBGColor,
+  paper_bgcolor: chartBGColor,
+  xaxis: {
+    color: chartAxisColor,
+    linecolor: chartAxisColor,
+    gridwidth: "2",
+    autorange: true,
+  },
+  yaxis: {
+    color: chartAxisColor,
+    linecolor: chartAxisColor,
+    gridwidth: "2",
+    autorange: true,
+  },
+};
+var humidityLayout = {
+  autosize: true,
+  title: {
+    text: "Humidity",
+  },
+  font: {
+    size: 12,
+    color: chartFontColor,
+    family: "poppins, san-serif",
+  },
+  colorway: ["#05AD86"],
+  margin: { t: 40, b: 40, l: 30, r: 30, pad: 0 },
+  plot_bgcolor: chartBGColor,
+  paper_bgcolor: chartBGColor,
+  xaxis: {
+    color: chartAxisColor,
+    linecolor: chartAxisColor,
+    gridwidth: "2",
+  },
+  yaxis: {
+    color: chartAxisColor,
+    linecolor: chartAxisColor,
+  },
+};
+
+var config = { responsive: true };
+
+window.addEventListener("load", (event) => {
+  Plotly.newPlot(
+    temperatureHistoryDiv,
+    [temperatureTrace],
+    temperatureLayout,
+    config
+  );
+  Plotly.newPlot(humidityHistoryDiv, [humidityTrace], humidityLayout, config);
+
+  // Run it initially
+  handleDeviceChange(mediaQuery);
+});
+
+// Gauge Data
+var temperatureData = [
+  {
+    domain: { x: [0, 1], y: [0, 1] },
+    value: 0,
+    title: { text: "Temperature" },
+    type: "indicator",
+    mode: "gauge+number+delta",
+    delta: { reference: 30 },
+    gauge: {
+      axis: { range: [null, 50] },
+      steps: [
+        { range: [0, 20], color: "lightgray" },
+        { range: [20, 30], color: "gray" },
+      ],
+      threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 30,
+      },
+    },
+  },
+];
+
+var humidityData = [
+  {
+    domain: { x: [0, 1], y: [0, 1] },
+    value: 0,
+    title: { text: "Humidity" },
+    type: "indicator",
+    mode: "gauge+number+delta",
+    delta: { reference: 50 },
+    gauge: {
+      axis: { range: [null, 100] },
+      steps: [
+        { range: [0, 20], color: "lightgray" },
+        { range: [20, 30], color: "gray" },
+      ],
+      threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 30,
+      },
+    },
+  },
+];
+
+var layout = { width: 300, height: 250, margin: { t: 0, b: 0, l: 0, r: 0 } };
+
+Plotly.newPlot(temperatureGaugeDiv, temperatureData, layout);
+Plotly.newPlot(humidityGaugeDiv, humidityData, layout);
+
+// Will hold the arrays we receive from our DHT22 sensor
+// Temperature
+let newTempXArray = [];
+let newTempYArray = [];
+// Humidity
+let newHumidityXArray = [];
+let newHumidityYArray = [];
+
+// The maximum number of data points displayed on our scatter/line graph
+let MAX_GRAPH_POINTS = 12;
+let ctr = 0;
+
+// Callback function that will retrieve our latest sensor readings and redraw our Gauge with the latest readings
+function updateSensorReadings(jsonResponse) {
+  console.log(typeof jsonResponse);
+  console.log(jsonResponse);
+
+  let temperature = Number(jsonResponse.temperature).toFixed(2);
+  let humidity = Number(jsonResponse.humidity).toFixed(2);
+
+  updateBoxes(temperature, humidity);
+
+  updateGauge(temperature, humidity);
+
+  // Update Temperature Line Chart
+  updateCharts(
+    temperatureHistoryDiv,
+    newTempXArray,
+    newTempYArray,
+    temperature
+  );
+  // Update Humidity Line Chart
+  updateCharts(
+    humidityHistoryDiv,
+    newHumidityXArray,
+    newHumidityYArray,
+    humidity
+  );
+}
+
+function updateBoxes(temperature, humidity) {
+  let temperatureDiv = document.getElementById("temperature");
+  let humidityDiv = document.getElementById("humidity");
+
+  temperatureDiv.innerHTML = temperature + " C";
+  humidityDiv.innerHTML = humidity + " %";
+}
+
+function updateGauge(temperature, humidity) {
+  var temperature_update = {
+    value: temperature,
+  };
+  var humidity_update = {
+    value: humidity,
+  };
+
+  Plotly.update(temperatureGaugeDiv, temperature_update);
+  Plotly.update(humidityGaugeDiv, humidity_update);
+}
+
+function updateCharts(lineChartDiv, xArray, yArray, sensorRead) {
+  if (xArray.length >= MAX_GRAPH_POINTS) {
+    xArray.shift();
+  }
+  if (yArray.length >= MAX_GRAPH_POINTS) {
+    yArray.shift();
+  }
+  xArray.push(ctr++);
+  yArray.push(sensorRead);
+
+  var data_update = {
+    x: [xArray],
+    y: [yArray],
+  };
+
+  Plotly.update(lineChartDiv, data_update);
+}
+
+function updateChartsBackground() {
+  // updates the background color of historical charts
+  var updateHistory = {
+    plot_bgcolor: chartBGColor,
+    paper_bgcolor: chartBGColor,
+    font: {
+      color: chartFontColor,
+    },
+    xaxis: {
+      color: chartAxisColor,
+      linecolor: chartAxisColor,
+    },
+    yaxis: {
+      color: chartAxisColor,
+      linecolor: chartAxisColor,
+    },
+  };
+  historyCharts.forEach((chart) => Plotly.relayout(chart, updateHistory));
+
+  // updates the background color of gauge charts
+  var gaugeHistory = {
+    plot_bgcolor: chartBGColor,
+    paper_bgcolor: chartBGColor,
+    font: {
+      color: chartFontColor,
+    },
+    xaxis: {
+      color: chartAxisColor,
+      linecolor: chartAxisColor,
+    },
+    yaxis: {
+      color: chartAxisColor,
+      linecolor: chartAxisColor,
+    },
+  };
+  gaugeCharts.forEach((chart) => Plotly.relayout(chart, gaugeHistory));
+}
+
+const mediaQuery = window.matchMedia("(max-width: 600px)");
+
+mediaQuery.addEventListener("change", function (e) {
+  handleDeviceChange(e);
+});
+
+function handleDeviceChange(e) {
+  if (e.matches) {
+    console.log("Inside Mobile");
+    var updateHistory = {
+      width: 323,
+      height: 250,
+      "xaxis.autorange": true,
+      "yaxis.autorange": true,
+    };
+    historyCharts.forEach((chart) => Plotly.relayout(chart, updateHistory));
+  } else {
+    var updateHistory = {
+      width: 550,
+      height: 260,
+      "xaxis.autorange": true,
+      "yaxis.autorange": true,
+    };
+    historyCharts.forEach((chart) => Plotly.relayout(chart, updateHistory));
+  }
+}
+
+function retrieveSensorReadings() {
+  fetch(`/sensorReadings`)
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      updateSensorReadings(jsonResponse);
+    });
+}
+
+// Continuos loop that runs evry 3 seconds to update our web page with the latest sensor readings
+(function loop() {
+  setTimeout(() => {
+    retrieveSensorReadings();
+    loop();
+  }, 3000);
+})();
